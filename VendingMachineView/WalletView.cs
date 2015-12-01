2a3eaf57eff843c14904e5df0e75f945958e4199
beetlewar <beetlewar@mail.ru>
2015-12-01 17:53:49 +0300

    1

---
+++ VendingMachineView/WalletView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VendingMachineEngine;
+
+namespace VendingMachineView
+{
+    public partial class WalletView : UserControl
+    {
+        public WalletView()
+        {
+            InitializeComponent();
+        }
+
+        public void Setup(IEnumerable<CoinsPile> piles)
+        {
+            foreach(var pile in piles)
+            {
+                var view = new CoinsPileInfoView();
+                view.Setup(pile);
+                this._panel.Controls.Add(view);
+            }
+        }
+    }
+}
